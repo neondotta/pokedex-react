8e906dd0c6d2b881a2d104ce7dddc3d5b543ea26
Neon Dotta <neondotta@gmail.com>
2023-05-22 17:59:24 -0300

    add filter by name in home page

--- src/pages/Home/Components/ListPokemons/index.tsx
+++ src/pages/Home/Components/ListPokemons/index.tsx
@@ -1,5 +1,5 @@
 import { useContextSelector } from "use-context-selector";
-import { CardsContainer, ContainerButtonMorePokemon, ListPokemonsContainer, TypePokemonBlock } from "./styles";
+import { CardsContainer, ContainerButtonMorePokemon, ErrorMessage, ListPokemonsContainer, TypePokemonBlock } from "./styles";
 
 import PokeballSVG from '../../../../assets/pokeball.svg'
 
@@ -19,33 +19,40 @@ export function ListPokemons() {
             {isLoading && (
                 <Loader type='spinningBubbles' isLoading={isLoading} />
             )}
-            <ListPokemonsContainer>
-                {pokemons.map((pokemon) => {
-                    return <CardsContainer type={pokemon.types[0].name} key={pokemon.id}>
-                        <NavLink to={`/pokemon/${pokemon.name.toLowerCase()}`}>
-                            <header>
-                                <img className="pokemon-image" src={ pokemon.images.default } alt="" />
-                                <img src={PokeballSVG} className="background-image" alt="" />
-                            </header>
-                            <main>
-                                <p className="number">#{pokemon.number}</p>
-                                <p className="name">{pokemon.name}</p>
-                                <div className="types">
-                                    {pokemon.types.map((currentType) => {
-                                        return <TypePokemonBlock key={currentType.name} type={currentType.name}>
-                                            {currentType.name}
-                                        </TypePokemonBlock>
-                                    })}                            
-                                </div>
-                            </main>
-                        </NavLink>
-                    </CardsContainer>
-                })}
-            </ListPokemonsContainer>
-            
-            <ContainerButtonMorePokemon>
-                <button onClick={() => fetchPokemons(nextUrl)}> Buscar mais Pokémons</button>
-            </ContainerButtonMorePokemon>
+
+            {pokemons.length > 0 ? (
+                <>
+                    <ListPokemonsContainer>
+                        {pokemons.map((pokemon) => {
+                            return <CardsContainer type={pokemon.types[0].name} key={pokemon.id}>
+                                <NavLink to={`/pokemon/${pokemon.name.toLowerCase()}`}>
+                                    <header>
+                                        <img className="pokemon-image" src={ pokemon.images.default } alt="" />
+                                        <img src={PokeballSVG} className="background-image" alt="" />
+                                    </header>
+                                    <main>
+                                        <p className="number">#{pokemon.number}</p>
+                                        <p className="name">{pokemon.name}</p>
+                                        <div className="types">
+                                            {pokemon.types.map((currentType) => {
+                                                return <TypePokemonBlock key={currentType.name} type={currentType.name}>
+                                                    {currentType.name}
+                                                </TypePokemonBlock>
+                                            })}                            
+                                        </div>
+                                    </main>
+                                </NavLink>
+                            </CardsContainer>
+                        })}
+                    </ListPokemonsContainer>
+                    
+                    <ContainerButtonMorePokemon>
+                        <button onClick={() => fetchPokemons(nextUrl)}> Buscar mais Pokémons</button>
+                    </ContainerButtonMorePokemon>
+                </>
+            ) : (
+                <ErrorMessage>Não foi encontrado nenhum pokémon</ErrorMessage>
+            )}
         </>
     )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,10 @@
 import { ListPokemons } from "./Components/ListPokemons";
+import { SearchForm } from "./Components/SearchForm";
 
 export function Home() {
     return (
         <>
+            <SearchForm />
             <ListPokemons />
         </>
     )
